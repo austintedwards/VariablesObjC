374733fda65d71cbe2951dd07316ae576ae402eb
Austintedwards <austintedwards@gmail.com>
2017-07-26 09:40:21 -0600

    Initial Commit

---
+++ VariablesObjC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VariablesObjC
+//
+//  Created by Austin Edwards on 7/26/17.
+//  Copyright © 2017 Austin Edwards. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
